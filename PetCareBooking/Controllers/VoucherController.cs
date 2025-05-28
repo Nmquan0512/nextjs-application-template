ae2038f0f21b8f7e68ec7de07cfa8b12945e0abd
Nmquan0512 <93432250+Nmquan0512@users.noreply.github.com>
2025-05-28 19:54:18 +0700

    Add PetCareBooking/Controllers/VoucherController.cs 1748436857901

---
+++ PetCareBooking/Controllers/VoucherController.cs
@@ -0,0 +1,146 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PetCareBooking.Data;
+using PetCareBooking.Models;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace PetCareBooking.Controllers
+{
+    public class VoucherController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VoucherController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Voucher
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Vouchers.ToListAsync());
+        }
+
+        // GET: Voucher/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var voucher = await _context.Vouchers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (voucher == null)
+            {
+                return NotFound();
+            }
+
+            return View(voucher);
+        }
+
+        // GET: Voucher/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Voucher/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Code,Description,DiscountAmount,ExpiryDate,IsActive")] Voucher voucher)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(voucher);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(voucher);
+        }
+
+        // GET: Voucher/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var voucher = await _context.Vouchers.FindAsync(id);
+            if (voucher == null)
+            {
+                return NotFound();
+            }
+            return View(voucher);
+        }
+
+        // POST: Voucher/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Code,Description,DiscountAmount,ExpiryDate,IsActive")] Voucher voucher)
+        {
+            if (id != voucher.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(voucher);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VoucherExists(voucher.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(voucher);
+        }
+
+        // GET: Voucher/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var voucher = await _context.Vouchers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (voucher == null)
+            {
+                return NotFound();
+            }
+
+            return View(voucher);
+        }
+
+        // POST: Voucher/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var voucher = await _context.Vouchers.FindAsync(id);
+            _context.Vouchers.Remove(voucher!);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VoucherExists(int id)
+        {
+            return _context.Vouchers.Any(e => e.Id == id);
+        }
+    }
+}
